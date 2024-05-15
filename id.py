1b11a2657da561938a73588939220ea69d035a3e
Mukhamedali27 <64990893+Mukhamedali27@users.noreply.github.com>
2024-05-15 11:50:18 +0500

    Update id.py

--- id.py
+++ id.py
@@ -77,4 +77,45 @@ for index, row in combined_df.iterrows():
     my_dict[row['POLICY_ID']] = row['Начисл.доход']
 
 # Вывод словаря
+
+
+
+
+
 print(my_dict)
+
+
+
+
+
+
+
+
+
+import pandas as pd
+
+# Список листов, которые нужно прочитать
+sheets = ['01032024 381', '1', '2']  # Замените на фактические имена листов
+
+# Загрузка данных из всех листов и объединение их в один DataFrame
+dfs = []
+for sheet in sheets:
+    df = pd.read_excel('C:\\Users\\zhumabek.m\\Desktop\\awd\\febral.xlsx', sheet_name=sheet, usecols=['POLICY_ID','Начисл.доход'])
+    dfs.append(df)
+
+# Объединение DataFrame'ов из разных листов
+combined_df = pd.concat(dfs, ignore_index=True)
+
+# Создание словаря для хранения сумм для каждого POLICY_ID
+sum_dict = {}
+for index, row in combined_df.iterrows():
+    policy_id = row['POLICY_ID']
+    income = row['Начисл.доход']
+    if policy_id in sum_dict:
+        sum_dict[policy_id].append(income)
+    else:
+        sum_dict[policy_id] = [income]
+
+# Вывод словаря
+print(sum_dict)
+
